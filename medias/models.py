142a2e5c38111e003977762c8eb5d2e05acd6a75
Caio Freitas <caio078freitas@gmail.com>
2025-08-23 19:17:13 -0300

    feat: add created_at field in Media model

--- medias/models.py
+++ medias/models.py
@@ -8,6 +8,7 @@ class Media(models.Model):
     image = models.URLField()
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
