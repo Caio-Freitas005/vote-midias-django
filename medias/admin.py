a6e9e2e75b2e6e09dd8216c583b8f1afc5857234
Caio Freitas <caio078freitas@gmail.com>
2025-08-23 18:05:54 -0300

    style: adjust spacing in code

--- medias/admin.py
+++ medias/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Media
 
+
 class MediaAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "genre", "description", "image"]
     search_fields = ["title", "genre"]
+
 
 admin.site.register(Media, MediaAdmin)
--- medias/views.py
+++ medias/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Media
 
+
 class MediasView(generic.ListView):
     template_name = "medias/medias.html"
     context_object_name = "medias_list"
-    
+
     def get_queryset(self):
         return Media.objects.all()
